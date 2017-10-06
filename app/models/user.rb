6987ccdddbaeddc648d590a4d7c16b072f2220a3
Kara <kara.a.flaherty@gmail.com>
2017-10-06 07:54:12 -0600

    Adjusted user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,12 @@ class User < ApplicationRecord
   has_many :lists, dependent: :destroy
 
   validates :username,
-            length: { minimum: 1, maximum: 100 },
             presence: true,
+            length: { minimum: 1, maximum: 100 },
             uniqueness: { case_sensitive: false }
   validates :password,
             presence: true,
-            length: { minimum: 6 },
-            if: "password_digest.nil?"
-  validates :password,
-            length: { minimum: 6 },
-            allow_blank: true
+            length: { minimum: 6 }
 
   has_secure_password
 end
